db464290480133977303faac6f70c7e054b952e4
sajidzaidi001 <sajidhussainzaidi1@gmail.com>
2023-09-13 06:26:49 -0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+console.log("Lets get a number b/w 1 to 10 in 03 tries");
+let actualAnswer: number =  Math.floor(Math.random()*10);
+let guessNumber: number = 7
+let numTries: number = 3;
+const answer= await inquirer.prompt({
+    
+        type: "number",
+        name: "userGuess",
+        message: "Enter your Guess Number"
+  });
+  
+  if(answer.userGuess==actualAnswer){
+    console.log("You guessed the right answer")
+  console.log(`You have ${numTries-1} left`)
+  
+  }else{
+    console.log("You are Wrong");
+    numTries--;
+  }
+  
+
